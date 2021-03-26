544cd1196a2692448306aabdecb76142aa526388
juacrumar <juacrumar@gmail.com>
2021-03-26 18:21:46 +0100

    add tests

--- src/alohaflow/phasespace.py
+++ src/alohaflow/phasespace.py
@@ -6,6 +6,7 @@
 """
 
 from .config import float_me, run_eager, DTYPE
+
 run_eager(True)
 
 import numpy as np
@@ -18,6 +19,8 @@ logger = logging.getLogger(__name__)
 
 # Helpers for rambo
 tf.TensorSpec(shape=[None, 4], dtype=DTYPE),
+
+
 def _gen_unconstrained_momenta(xrand):
     """
     Generates unconstrained 4-momenta
@@ -34,12 +37,13 @@ def _gen_unconstrained_momenta(xrand):
     costh = 2.0 * xrand[:, 0] - 1.0
     sinth = tf.sqrt(1.0 - costh ** 2)
     phi = 2 * PI * xrand[:, 1]
-    energy = -1.0*tf.math.log(tf.reduce_prod(xrand[:, 2:4], axis=1))
+    energy = -1.0 * tf.math.log(tf.reduce_prod(xrand[:, 2:4], axis=1))
 
     qx = energy * sinth * tf.math.sin(phi)
     qy = energy * sinth * tf.math.cos(phi)
     qz = energy * costh
     return tf.stack([energy, qx, qy, qz], axis=1)
+
 
 def rambo(xrand, n_particles, sqrts, masses=None, check_physical=False):
     """
@@ -65,12 +69,12 @@ def rambo(xrand, n_particles, sqrts, masses=None, check_physical=False):
             masses = float_me(masses)
 
     # Consume the random numbers into unconstrained momenta that will be later transformed
-    all_q = [_gen_unconstrained_momenta(xrand[:, i*4:(i+1)*4]) for i in range(n_particles)]
-    sum_q = tf.reduce_sum(all_q, axis=0) # (n_events, 4)
-    sum_q2 = sum_q**2
-    qmass = tf.sqrt(sum_q2[:,0:1] - tf.reduce_sum(sum_q2[:,1:], axis=1, keepdims=1))
-    x = sqrts / qmass # (nevents, 1)
-    bquad = -sum_q / qmass # (nevents, 4)
+    all_q = [_gen_unconstrained_momenta(xrand[:, i * 4 : (i + 1) * 4]) for i in range(n_particles)]
+    sum_q = tf.reduce_sum(all_q, axis=0)  # (n_events, 4)
+    sum_q2 = sum_q ** 2
+    qmass = tf.sqrt(sum_q2[:, 0:1] - tf.reduce_sum(sum_q2[:, 1:], axis=1, keepdims=1))
+    x = sqrts / qmass  # (nevents, 1)
+    bquad = -sum_q / qmass  # (nevents, 4)
     bvec = bquad[:, 1:]
     gamma = -bquad[:, 0:1]
     a = 1.0 / (1.0 + gamma)
@@ -78,20 +82,20 @@ def rambo(xrand, n_particles, sqrts, masses=None, check_physical=False):
     # Perform the conformal transformation q -> p
     all_p = []
     for q in all_q:
-        bq = tf.reduce_sum(q[:, 1:]*bvec, axis=1, keepdims=True)
-        tmp = bq*a + q[:, 0:1]
-        pvec = (q[:,1:] + bvec*tmp) # (n_events, 3)
-        pnrg = q[:,0:1]*gamma + bq
-        p = tf.concat([pnrg, pvec], axis=1)*x # (n_events, 4)
+        bq = tf.reduce_sum(q[:, 1:] * bvec, axis=1, keepdims=True)
+        tmp = bq * a + q[:, 0:1]
+        pvec = q[:, 1:] + bvec * tmp  # (n_events, 3)
+        pnrg = q[:, 0:1] * gamma + bq
+        p = tf.concat([pnrg, pvec], axis=1) * x  # (n_events, 4)
         all_p.append(p)
-    all_p = tf.stack(all_p, axis=1) # (n_events, n_particles, 4)
+    all_p = tf.stack(all_p, axis=1)  # (n_events, n_particles, 4)
 
     # Finally compute the weight for the phase space point
-    wt = tf.math.log(PI/2.0)*(n_particles-1)
-    wt -= 2.0*tf.math.lgamma(float_me(n_particles-1))
-    wt -= tf.math.log(float_me(n_particles-1))
-    wt += (2*n_particles - 4)*tf.math.log(sqrts)
+    wt = tf.math.log(PI / 2.0) * (n_particles - 1)
+    wt -= 2.0 * tf.math.lgamma(float_me(n_particles - 1))
+    wt -= tf.math.log(float_me(n_particles - 1))
+    wt += (2 * n_particles - 4) * tf.math.log(sqrts)
 
     if masses is None:
-        wt = tf.exp(wt)/tf.pow(2*PI, 3*n_particles-4)
-        return all_p, tf.ones_like(x)*wt
+        wt = tf.exp(wt) / tf.pow(2 * PI, 3 * n_particles - 4)
+        return all_p, tf.ones_like(x) * wt
