0fd2755d6fa5e51759b3479bccb40c6fc3038e79
juacrumar <juacrumar@gmail.com>
2021-03-29 18:32:35 +0200

    now it should work

--- src/alohaflow/tests/test_integration.py
+++ src/alohaflow/tests/test_integration.py
@@ -2,22 +2,20 @@
     Test for the integration routine
 """
 
-import pathlib
+import os
 import numpy as np
 from alohaflow.utilities import one_matrix_integration
 from pdfflow import mkPDF
 
 # For github actions, check whether we find a PDF set directorty
-git_pdfs = pathlib.Path("/tmp/LHAPDF")
-if not git_pdfs.exists():
-    git_pdfs = None
+git_pdfs = os.environ.get("PDFDIR")
 
 def test_integration():
     """ Regresion-style check to the integration routine using a predefined
     mockup matrix element.
     Checks that the result is within 3 sigmas of the true result
     """
-    from mockup_debug_me import Matrix_1_gg_ttx, model_params
+    from .mockup_debug_me import Matrix_1_gg_ttx, model_params
     matrix = Matrix_1_gg_ttx()
     pdf = mkPDF("NNPDF31_nnlo_as_0118/0", dirname=git_pdfs)
     res, error = one_matrix_integration(matrix, model_params, pdf=pdf, flavours=(0,), out_masses=[173.0, 173.0])
