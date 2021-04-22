acb3c9d9dbc67517ac253004b831b475cee4377e
Juan M. Cruz-Martinez <juacrumar@lairen.eu>
2021-04-22 16:55:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 """
 
 from setuptools import setup, find_packages
+from sys import version_info
 
 requirements = ['vegasflow']
 if version_info.major >=3 and version_info.minor >= 9:
@@ -14,11 +15,19 @@ requirements.append(tf_pack)
 package_name = "alohaflow"
 description = "Tensorized verson of ALOHA package"
 
-version = "0.01"  # TODO
-
+def get_version():
+    """ Gets the version from the package's __init__ file
+    if there is some problem, let it happily fail """
+    VERSIONFILE = os.path.join('src', PACKAGE, '__init__.py')
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return mo.group(1)
 setup(
     name=package_name,
-    version=version,
+    version=get_version(),
     description=description,
     author="S. Carrazza, J. Cruz-Martinez, M. Rossi, M. Zaro",
     author_email="https://github.com/N3PDF/pyoutformg5amc/issues/new",
