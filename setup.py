b17b776f480a75f1d853fa96c42f53dd7c09e577
juacrumar <juacrumar@gmail.com>
2021-05-06 15:09:28 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
     Installation script for alohaflow (WIP)
 """
 
-from setuptools import setup, find_packages
+from pathlib import Path
 from sys import version_info
+import re
+from setuptools import setup, find_packages
 
-requirements = ['vegasflow']
-if version_info.major >=3 and version_info.minor >= 9:
+requirements = ["vegasflow"]
+if version_info.major >= 3 and version_info.minor >= 9:
     # For python above 3.9 the only existing TF is 2.5 which works well (even pre releases)
     tf_pack = "tensorflow"
 else:
@@ -15,16 +17,19 @@ requirements.append(tf_pack)
 package_name = "alohaflow"
 description = "Tensorized verson of ALOHA package"
 
+
 def get_version():
-    """ Gets the version from the package's __init__ file
-    if there is some problem, let it happily fail """
-    VERSIONFILE = os.path.join('src', PACKAGE, '__init__.py')
-    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    """Gets the version from the package's __init__ file
+    if there is some problem, let it happily fail"""
+    version_file = Path(f"src/{package_name}/__init__.py")
+    initfile_lines = version_file.open("rt").readlines()
     VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
     for line in initfile_lines:
         mo = re.search(VSRE, line, re.M)
         if mo:
             return mo.group(1)
+
+
 setup(
     name=package_name,
     version=get_version(),
@@ -42,6 +47,6 @@ setup(
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Physics",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=requirements,
 )
